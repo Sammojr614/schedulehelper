963cefa8ad83162407a5c2dac63920251e794744
Sammojr614 <Sammojr614@gmail.com>
2022-03-15 14:41:03 -0400

    Creating another window

--- src/App.java
+++ src/App.java
@@ -1,20 +1,44 @@
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.CloseAction;
+
 import org.json.simple.*;
 import org.json.simple.parser.*;
 
 public class App {
+    
     public static void main(String[] args) throws Exception {
+      
         var loadGui = new gui();
         loadGui.setVisible(true);
     }
 }
 class readsched extends JFrame{
     readsched(){
+        initGui();
+    }
+    private void initGui(){
+        createMenuBar();
+        setTitle("Edit Schedule");
+        setSize(320,320);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
+    private void createMenuBar(){
+        var menubar = new JMenuBar();
+        var fileMenu = new JMenu("File");
+        var eMenuItem = new JMenuItem("Exit");
+
+        eMenuItem.setToolTipText("Exit App");
+
+        fileMenu.add(eMenuItem);
+        menubar.add(fileMenu);
+
+        setJMenuBar(menubar);
 
     }
 
 }
 class gui extends JFrame {
+    readsched reader = new readsched();
     gui() {
         initGui();
     }
@@ -38,6 +62,7 @@ class gui extends JFrame {
         eMenuItem.addActionListener((event) -> System.exit(1));
         var edMenuItem = new JMenuItem("Add");
         edMenuItem.setToolTipText("Set Things");
+        edMenuItem.addActionListener((event) -> reader.setVisible(true));
         
 
         fileMenu.add(eMenuItem);
