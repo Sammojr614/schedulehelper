f85e0d4cf8e4e625db436e3c4d6ea0aa3ed36c79
Sammojr614 <Sammojr614@gmail.com>
2022-03-15 14:01:34 -0400

    gui made

--- src/App.java
+++ src/App.java
@@ -1,5 +1,50 @@
+import javax.swing.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var loadGui = new gui();
+        loadGui.setVisible(true);
+    }
+}
+class readsched extends JFrame{
+    readsched(){
+        
+    }
+}
+class gui extends JFrame {
+    gui() {
+        initGui();
+    }
+
+    private void initGui() {
+        createMenuBar();
+
+        setTitle("Schedule Helper");
+        setSize(320,320);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    private void createMenuBar(){
+        var menubar = new JMenuBar();
+        var fileMenu = new JMenu("File");
+        var exitIcon =  new ImageIcon("src/resources/exit.png");
+        var editMenu =  new JMenu("Edit");
+        
+
+        var eMenuItem = new JMenuItem("Exit", exitIcon);
+        eMenuItem.setToolTipText("Exit App");
+        eMenuItem.addActionListener((event) -> System.exit(1));
+        var edMenuItem = new JMenuItem("Add");
+        edMenuItem.setToolTipText("Set Things");
+        
+
+        fileMenu.add(eMenuItem);
+        editMenu.add(edMenuItem);
+        menubar.add(fileMenu);
+        menubar.add(editMenu);
+        
+
+        setJMenuBar(menubar);
+        
+
     }
 }
