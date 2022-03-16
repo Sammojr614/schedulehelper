6d4170499f23b5acdbb0c0a7fe0459da6327ed6c
Sammmojr <sammojr614@gmail.com>
2022-03-16 13:58:31 -0400

    Attempted to add pannel

--- src/App.java
+++ src/App.java
@@ -4,17 +4,41 @@ import javax.swing.plaf.basic.BasicInternalFrameTitlePane.CloseAction;
 import org.json.simple.*;
 import org.json.simple.parser.*;
 
+import java.io.FileReader;
+import java.util.*;
+
 public class App {
-    
+    public static List<String> names = new ArrayList<>();
+    public static List<String> classNames = new ArrayList<>();
+    public static List<String> StartTimes = new ArrayList<>();
+
     public static void main(String[] args) throws Exception {
       
-        var loadGui = new gui();
+        gui loadGui = new gui();
         loadGui.setVisible(true);
     }
 }
 class readsched extends JFrame{
+
     readsched(){
         initGui();
+    }
+    private void readFile(){
+        JSONParser parser = new JSONParser();
+        JLabel label =  new JLabel();
+            try{
+
+                JSONObject obj = (JSONObject) parser.parse(new FileReader("src/schedule.json"));
+                JSONArray arr = (JSONArray) obj.get("assigments");
+
+                for(Object o: arr){
+                    JSONObject getName = (JSONObject) o;
+                    String name = (String) getName.get("nameOf");
+                    App.names.add(name);
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
     }
     private void initGui(){
         createMenuBar();
@@ -37,15 +61,25 @@ class readsched extends JFrame{
     }
 
 }
+class guiActionArea extends JPanel{
+    gui mainGui = new gui();
+    guiActionArea(){
+    initActionArea();
+    }
+    private void initActionArea(){
+        setSize(mainGui.getWidth(),mainGui.getHeight());
+    }
+}
 class gui extends JFrame {
     readsched reader = new readsched();
+    guiActionArea actionArea = new guiActionArea();
     gui() {
         initGui();
     }
 
     private void initGui() {
         createMenuBar();
-
+        actionArea.setVisible(true);
         setTitle("Schedule Helper");
         setSize(320,320);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
