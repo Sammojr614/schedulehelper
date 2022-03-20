3a58574b102d98ea6aab2276cda46e8c6f0d22c4
Sammmojr <sammojr614@gmail.com>
2022-03-19 20:53:45 -0400

    Trying to add time

--- src/App.java
+++ src/App.java
@@ -1,57 +1,68 @@
 import javax.swing.*;
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane.CloseAction;
+import java.lang.*;
 
 import org.json.simple.*;
 import org.json.simple.parser.*;
 
 import java.awt.*;
 import java.io.FileReader;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.List;
+import java.time.*;
 
 public class App {
     public static List<String> names = new ArrayList<>();
     public static List<String> classNames = new ArrayList<>();
-    public static List<String> StartTimes = new ArrayList<>();
+    public static List<String> startTimes = new ArrayList<>();
+    public static List<String> endTimes = new ArrayList<>();
+    public static  Boolean isRunning = false;
 
     public static void main(String[] args) throws Exception {
-      
+
         gui loadGui = new gui();
         loadGui.setVisible(true);
     }
 }
 class readsched extends JFrame{
-
+    JPanel actionArea = new JPanel();
     readsched(){
         initGui();
     }
     private void readFile(){
         JSONParser parser = new JSONParser();
-        JLabel label =  new JLabel();
             try{
 
-                JSONObject obj = (JSONObject) parser.parse(new FileReader("src/schedule.json"));
-                JSONArray arr = (JSONArray) obj.get("assigments");
-
-                for(Object o: arr){
-                    JSONObject getName = (JSONObject) o;
-                    String name = (String) getName.get("nameOf");
-                    App.names.add(name);
+                JSONObject obj = (JSONObject) parser.parse(new FileReader("C:/Users/sammo/Desktop/Schedue/schedulehelper/src/schedule.json"));
+                JSONArray arr = (JSONArray) obj.get("assignments");
+                for(Object o: arr) {
+                JSONObject getName = (JSONObject) o;
+                String name = (String) getName.get("nameOf");
+                String className = (String) getName.get("className");
+                String startTimes = (String) getName.get("startTime");
+                String endTimes  = (String)getName.get("endTime");
+                App.classNames.add(className);
+                App.names.add(name);
+                App.startTimes.add(startTimes);
+                App.endTimes.add(endTimes);
                 }
             }catch(Exception e){
                 e.printStackTrace();
             }
     }
     private void initGui(){
+        readFile();
         createMenuBar();
         setTitle("Edit Schedule");
         setSize(320,320);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setContentPane(actionArea);
     }
     private void createMenuBar(){
-        var menubar = new JMenuBar();
-        var fileMenu = new JMenu("File");
-        var eMenuItem = new JMenuItem("Exit");
+        JMenuBar menubar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem eMenuItem = new JMenuItem("Exit");
 
         eMenuItem.setToolTipText("Exit App");
 
@@ -63,11 +74,30 @@ class readsched extends JFrame{
     }
 
 }
+class TellTime{
+   static int hour,minutes,seconds;
+    String time24;
+    public TellTime(){
+        Calendar cal = Calendar.getInstance();
 
+        while(true){
+            seconds = cal.get(Calendar.SECOND);
+            if(seconds >= 59){
+                hour = cal.get(Calendar.HOUR_OF_DAY);
+                minutes = cal.get(Calendar.MINUTE);
+                SimpleDateFormat sdf12 = new SimpleDateFormat("hh:mm aa");
+                Date dat = cal.getTime();
+                time24 = sdf12.format(dat);
+            }
+        }
+
+
+    }
+}
 
 class gui extends JFrame {
     readsched reader = new readsched();
-    JPanel guiActionArea = new JPanel(new BorderLayout());
+    JPanel guiActionArea = new JPanel(null);
 
     gui() {
         initGui();
@@ -75,27 +105,37 @@ class gui extends JFrame {
 
     private void initGui() {
         createMenuBar();
-        initActionArea();
         setTitle("Schedule Helper");
-        setSize(320,320);
+        setSize(1280,800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        initActionArea();
     }
     private void initActionArea(){
       setContentPane(guiActionArea);
-      JButton testButton = new JButton();
-      guiActionArea.add(testButton, BorderLayout.WEST);
+        TellTime clock = new TellTime();
+      JLabel TimeLabel = new JLabel();
+      TimeLabel.setText(clock.time24);
+      TimeLabel.setBounds(10,10,1280,300);
+      TimeLabel.setFont(TimeLabel.getFont().deriveFont(90.0f));
+      guiActionArea.add(TimeLabel);
+
+
+
+
+
+
     }
     private void createMenuBar(){
-        var menubar = new JMenuBar();
-        var fileMenu = new JMenu("File");
-        var exitIcon =  new ImageIcon("src/resources/exit.png");
-        var editMenu =  new JMenu("Edit");
+        JMenuBar menubar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        ImageIcon exitIcon =  new ImageIcon("src/resources/exit.png");
+        JMenu editMenu =  new JMenu("Edit");
         
 
-        var eMenuItem = new JMenuItem("Exit", exitIcon);
+        JMenuItem eMenuItem = new JMenuItem("Exit", exitIcon);
         eMenuItem.setToolTipText("Exit App");
         eMenuItem.addActionListener((event) -> System.exit(1));
-        var edMenuItem = new JMenuItem("Add");
+        JMenuItem edMenuItem = new JMenuItem("Add");
         edMenuItem.setToolTipText("Set Things");
         edMenuItem.addActionListener((event) -> reader.setVisible(true));
         
